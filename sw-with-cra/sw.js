e7f45b9b7f988b0bbfec13e35096cb1e8b41f2fc
kyun <kyun@superlabs.us>
2022-11-12 11:34:34 +0900

    init

---
+++ sw-with-cra/sw.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-restricted-globals */
+// This code executes in its own worker or thread
+self.addEventListener("install", (e) => {
+  console.log("service worker installed");
+});
+self.addEventListener("activate", (e) => {
+  console.log("service worker activated");
+});
